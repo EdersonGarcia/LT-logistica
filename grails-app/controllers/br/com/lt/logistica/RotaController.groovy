7cedbd280ae25cc9627c8c7028f7baeea4e47b92
Ederson <edersongarcia@outlook.com>
2015-02-16 15:06:11 -0200

    remocao command object

--- grails-app/controllers/br/com/lt/logistica/RotaController.groovy
+++ grails-app/controllers/br/com/lt/logistica/RotaController.groovy
@@ -2,6 +2,8 @@ package br.com.lt.logistica
 
 import grails.converters.JSON
 
+import java.beans.Transient
+
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
@@ -9,90 +11,42 @@ import grails.transaction.Transactional
 class RotaController {
     def ltService
     static scaffold = true
-    static responseFormats = ['json', 'xml']
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static responseFormats = ['json', 'xml']
 
-    @Transactional
-    def save(Rota rotaInstance) {
-        if (rotaInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        rotaInstance.validate()
-        if (rotaInstance.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-        rotaInstance.save flush:true
-        respond rotaInstance, [status: CREATED]
-    }
-
-    @Transactional
-    def update(Rota rotaInstance) {
-        if (rotaInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        rotaInstance.validate()
-        if (rotaInstance.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-        rotaInstance.save flush:true
-        respond rotaInstance, [status: OK]
-    }
-
-    @Transactional
-    def delete(Rota rotaInstance) {
-
-        if (rotaInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        rotaInstance.delete flush:true
-        render status: NO_CONTENT
-    }
 
     @Transactional(readOnly = true)
-       def melhorRota(MelhorRotaCommand melhorRotaCommand){
-        if(melhorRotaCommand.validate()) {
-            Localizacao origem , destino
-            origem = ltService.buscarLocalizacaoPorNome(melhorRotaCommand.mapa,melhorRotaCommand.origem)
-            destino = ltService.buscarLocalizacaoPorNome(melhorRotaCommand.mapa,melhorRotaCommand.destino)
+       def melhorRota(String mapa,String origem , String destino, Integer autonomia , Double valorCombustivel){
+        println(mapa)
+        if(!origem.isEmpty() && !destino.isEmpty() &&
+                autonomia>0  && valorCombustivel>0 && !mapa.isEmpty()) {
+            Localizacao origemObj , destinoObj
+
+             origemObj = ltService.buscarLocalizacaoPorNome(mapa,origem)
+            destinoObj = ltService.buscarLocalizacaoPorNome(mapa,destino)
             if(origem!=null && destino!=null) {
-                def melhorRota = ltService.melhorRota(origem, destino)
-                melhorRota << [custo:ltService.calculaCustoRota(melhorRota.distancia.toInteger(),melhorRotaCommand.autonomia,melhorRotaCommand.valorCombustivel)]
+                def melhorRota = ltService.melhorRota(origemObj, destinoObj)
+                melhorRota << [custo:ltService.calculaCustoRota(melhorRota.distancia.toInteger(),autonomia,valorCombustivel)]
                 render([melhorRota]) as JSON
                 }else{
+
                 render (status:NOT_ACCEPTABLE )
             }
         }else{
+
             render (status:NOT_ACCEPTABLE )
         }
     }
 
+    def salvarRota(String mapa, String origem , String destino, int kilometragem ){
+        if(mapa == null || origem == null|| destino == null || kilometragem <=0){
+            respond(info:'parametros invalidos')
+            println(params)
+            return
+        }
+        def rota = ltService.salvarRota(mapa,origem,destino,kilometragem)
+        respond([id:rota.id,origem:rota.origem.nome,destino:rota.destino.nome,kilometragem:rota.kilometragem]) as JSON
+    }
 
 }
-@grails.validation.Validateable
-class MelhorRotaCommand{
-    String mapa
-    String origem
-    String destino
-    Integer autonomia
-    Double valorCombustivel
 
-    static constraints = {
-        mapa blank: false
-        destino blank: false
-        autonomia blank: false, min: 0
-        valorCombustivel blank: false , min: 0D
-    }
-    String toString(){
-        return "$mapa - $destino - $destino - $autonomia - $valorCombustivel"
-    }
-}
