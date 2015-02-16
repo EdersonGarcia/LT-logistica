00c67faa2ac4dcdf7f19b24e1d779924ccc963e8
Ederson <edersongarcia@outlook.com>
2015-02-16 15:05:33 -0200

    Definido padrao controller rest grails

--- grails-app/controllers/br/com/lt/logistica/LocalizacaoController.groovy
+++ grails-app/controllers/br/com/lt/logistica/LocalizacaoController.groovy
@@ -1,11 +1,16 @@
 package br.com.lt.logistica
 
-
+import grails.rest.RestfulController
 import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
-class LocalizacaoController {
+class LocalizacaoController extends RestfulController {
     static scaffold = true
-
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    LocalizacaoController() {
+        super(Localizacao)
     }
+}
+
+
 
--- grails-app/controllers/br/com/lt/logistica/MapaController.groovy
+++ grails-app/controllers/br/com/lt/logistica/MapaController.groovy
@@ -1,10 +1,14 @@
 package br.com.lt.logistica
 
-
+import grails.rest.RestfulController
 import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
+class MapaController extends RestfulController {
+    static scaffold = true
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    MapaController() {
+            super(Mapa)
+        }
+    }
 
-class MapaController {
-    static scaffold =true
-}
