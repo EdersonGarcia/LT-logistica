06fc43ecb0fde7e773b523c9680467de4fd1d789
Ederson <edersongarcia@outlook.com>
2015-02-16 03:48:03 -0200

    criacao action MelhorRota

--- grails-app/controllers/br/com/lt/logistica/RotaController.groovy
+++ grails-app/controllers/br/com/lt/logistica/RotaController.groovy
@@ -25,7 +25,7 @@ class RotaController {
             return
         }
 
-        rotaInstance.save flush: true
+        rotaInstance.save flush:true
         respond rotaInstance, [status: CREATED]
     }
 
@@ -42,7 +42,7 @@ class RotaController {
             return
         }
 
-        rotaInstance.save flush: true
+        rotaInstance.save flush:true
         respond rotaInstance, [status: OK]
     }
 
@@ -54,8 +54,45 @@ class RotaController {
             return
         }
 
-        rotaInstance.delete flush: true
+        rotaInstance.delete flush:true
         render status: NO_CONTENT
     }
 
+    @Transactional(readOnly = true)
+       def melhorRota(MelhorRotaCommand melhorRotaCommand){
+        if(melhorRotaCommand.validate()) {
+            Localizacao origem , destino
+            origem = ltService.buscarLocalizacaoPorNome(melhorRotaCommand.mapa,melhorRotaCommand.origem)
+            destino = ltService.buscarLocalizacaoPorNome(melhorRotaCommand.mapa,melhorRotaCommand.destino)
+            if(origem!=null && destino!=null) {
+                def melhorRota = ltService.melhorRota(origem, destino)
+                melhorRota << [custo:ltService.calculaCustoRota(melhorRota.distancia.toInteger(),melhorRotaCommand.autonomia,melhorRotaCommand.valorCombustivel)]
+                render([melhorRota]) as JSON
+                }else{
+                render (status:NOT_ACCEPTABLE )
+            }
+        }else{
+            render (status:NOT_ACCEPTABLE )
+        }
+    }
+
+
+}
+@grails.validation.Validateable
+class MelhorRotaCommand{
+    String mapa
+    String origem
+    String destino
+    Integer autonomia
+    Double valorCombustivel
+
+    static constraints = {
+        mapa blank: false
+        destino blank: false
+        autonomia blank: false, min: 0
+        valorCombustivel blank: false , min: 0D
+    }
+    String toString(){
+        return "$mapa - $destino - $destino - $autonomia - $valorCombustivel"
+    }
 }
