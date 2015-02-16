e6c3812d192e3ff2f7bdbe169f19deeea39232cb
Ederson <edersongarcia@outlook.com>
2015-02-16 17:38:37 -0200

    Documentacao API

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Leva e Traz API",
+  "version": "0.0.1",
+  "description": "APIs de Logistica",
+  "title": "Leva E Traz API",
+  "url": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-02-16T19:37:09.693Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.1"
+  }
+});
